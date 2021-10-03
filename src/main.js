5c204b5b9722c87838509fdcecfcc931e5e2303d
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-03 11:41:24 +0330

    feat: a challenge and learn nested template

--- src/main.js
+++ src/main.js
@@ -2,7 +2,28 @@ console.log("app.js is running");
 
 // JSX - JavaScript XML
 
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>this is some info</p>
+        <ol>
+            <li>item one</li>
+            <li>item two</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Mohammad Mohsen Hosseini</h1>
+        <p>Age: 20</p>
+        <p>Location: Shiraz</p>
+    </div>
+);
+
+var appRootTwo = document.getElementById("challenge");
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRootTwo);
