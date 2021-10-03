197864be612a32c4c6bd18e59b3fbcad92f7c215
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-03 11:23:36 +0330

    feat: add babel configurations

---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log("app.js is running");
+
+// JSX - JavaScript XML
+
+var template = <h1>Indecision App</h1>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
