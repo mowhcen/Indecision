8cdcf42ffed16618af772e8646573ed5ffcc2f5b
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-03 12:56:29 +0330

    feat: challenge and use conditanal JSX

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 console.log("app.js is running");
 
-// JSX - JavaScript XML
+// if statements
+// ternary operator
+// logical and operates
 
 var app = {
     title: "My Indecision App",
     subTitle: "Let`s Machine Decision for you",
+    options: ["one", "two"],
 };
 
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subTitle}</p>
+        {app.subTitle && <p>{app.subTitle}</p>}
+        {app.options.length > 0 ? (
+            <p>Here is your options</p>
+        ) : (
+            <p>No options</p>
+        )}
         <ol>
             <li>item one</li>
             <li>item two</li>
@@ -20,15 +28,21 @@ var template = (
 
 var user = {
     name: "Mohammad Mohsen Hosseini",
-    age: 27,
+    age: 20,
     location: "Shiraz",
 };
 
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : "Anonymous"}</h1>
+        {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
