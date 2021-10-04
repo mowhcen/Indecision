53d8241891313d8cb6f5b5b4c3cc36adc2a55928
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-04 10:57:46 +0330

    refactor: move some practice file to playground

--- src/main.js
+++ src/main.js
@@ -26,36 +26,6 @@ const template = (
     </div>
 );
 
-let count = 0;
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
-
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
-
 const appRoot = document.getElementById("app");
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <button onClick={reset}>Reset</button>
-            <h1>Count: {count}</h1>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={addOne}>+1</button>
-        </div>
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);
