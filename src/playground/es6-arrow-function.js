890d2874d7d084d3584b1c429bb2c2e873c55bea
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-04 09:48:43 +0330

    feat: a challenge and review arrow functions

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,11 @@
+// const square = function (x) {
+//     return x * x;
+// };
+
+// const squareArrow = (x) => x * x;
+
+// console.log(square(8), squareArrow(6));
+
+const getFirstName = (fullName) => fullName.split(" ")[0];
+
+console.log(getFirstName("mohammad mohsen"));
