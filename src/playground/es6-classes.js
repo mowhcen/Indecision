fda28cf80186a10bbf7a1f1ad5d9537190c84a10
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-04 16:16:36 +0330

    feat: a challange and review sub classes

--- src/playground/es6-classes.js
+++ src/playground/es6-classes.js
@@ -13,10 +13,52 @@ class Person {
     }
 }
 
-const me = new Person("Mohammad Mohsen Hosseini", 18);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
 
-console.log(me.getDescription());
+    hasMajor() {
+        return !!this.major;
+    }
 
-const other = new Person();
+    getDescription() {
+        let description = super.getDescription();
 
-console.log(other.getDescription());
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.hasHomeLocation()) {
+            return greeting + ` I'm visiting from ${this.homeLocation}.`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveler("Mohammad Mohsen Hosseini", 18, "Shiraz");
+
+console.log(me.getGreeting());
+
+const other = new Traveler();
+
+console.log(other.getGreeting());
