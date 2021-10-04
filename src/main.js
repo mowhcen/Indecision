3a5f507a3987c88717628e49fc69cc0b50c905d3
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-04 11:15:22 +0330

    feat: two challange and learn form submit

--- src/main.js
+++ src/main.js
@@ -7,25 +7,51 @@ console.log("app.js is running");
 const app = {
     title: "My Indecision App",
     subTitle: "Let`s Machine Decision for you",
-    options: ["one", "two"],
+    options: [],
 };
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subTitle && <p>{app.subTitle}</p>}
-        {app.options.length > 0 ? (
-            <p>Here is your options</p>
-        ) : (
-            <p>No options</p>
-        )}
-        <ol>
-            <li>item one</li>
-            <li>item two</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        renderApp();
+        e.target.elements.option.value = "";
+    }
+};
+
+const rerender = () => {
+    app.options = [];
+
+    renderApp();
+};
 
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subTitle && <p>{app.subTitle}</p>}
+            <p>
+                {app.options.length > 0 ? "Here is your options" : "No options"}
+            </p>
+            <p>{app.options.length}</p>
+            <button onClick={rerender}>Remove All</button>
+            <ol>
+                <li>item one</li>
+                <li>item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
