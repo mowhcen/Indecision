4e6eca1e031e6a078099b77c97e819a0374c0955
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-09 15:33:36 +0330

    feat: learn how to update DOM

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -4,23 +4,38 @@ class Counter extends React.Component {
         this.handleAddOne = this.handleAddOne.bind(this);
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0,
+        };
     }
     handleAddOne() {
-        console.log("HandleAddOne");
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1,
+            };
+        });
     }
 
     handleMinusOne() {
-        console.log("HandleMinusOne");
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1,
+            };
+        });
     }
 
     handleReset() {
-        console.log("HandleReset");
+        this.setState(() => {
+            return {
+                count: 0,
+            };
+        });
     }
     render() {
         return (
             <div>
                 <button onClick={this.handleReset}>Reset</button>
-                <h1>Count: </h1>
+                <h1>Count: {this.state.count}</h1>
                 <button onClick={this.handleMinusOne}>-1</button>
                 <button onClick={this.handleAddOne}>+1</button>
             </div>
