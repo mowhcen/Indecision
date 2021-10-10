5c516da8cde67eece2207580b87c325048906a0a
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-10 09:05:13 +0330

    feat: section 4 finished

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ class IndecisionApp extends React.Component {
         super(props);
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
         this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: ["thing one", "thing two", "thing three"],
+            options: [],
         };
     }
 
@@ -21,6 +22,19 @@ class IndecisionApp extends React.Component {
         const option = this.state.options[randomNum];
         alert(option);
     }
+
+    handleAddOption(option) {
+        if (!option) {
+            return "Enter valid value to add item";
+        } else if (this.state.options.indexOf(option) > -1) {
+            return "This option already exists";
+        }
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat([option]),
+            };
+        });
+    }
     render() {
         const title = "My Indecision App";
         const subTitle = "Let`s Machine Decision for you";
@@ -36,7 +50,7 @@ class IndecisionApp extends React.Component {
                     options={this.state.options}
                     handleDeleteOptions={this.handleDeleteOptions}
                 />
-                <AddOption />
+                <AddOption handleAddOption={this.handleAddOption} />
             </div>
         );
     }
@@ -88,20 +102,30 @@ class Option extends React.Component {
     }
 }
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined,
+        };
+    }
     handleAddOption(e) {
         e.preventDefault();
 
         const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
 
-        if (option) {
-            alert(option);
-            e.target.elements.option.value = "";
-        }
+        this.setState(() => {
+            return {
+                error,
+            };
+        });
     }
 
     render() {
         return (
             <div>
+                {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" />
                     <button>Add Option</button>
