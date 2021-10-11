3a2bc019308971730f2eb849bafec7d597a76f58
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-11 09:48:00 +0330

    feat: remove output file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const template = <p>testJSX</p>;
+const template = <p>test JSX</p>;
 
 ReactDOM.render(template, document.getElementById("app"));
