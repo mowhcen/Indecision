713d62eeaa778a0f76867080266cd5ff8ed9c789
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-11 10:45:06 +0330

    feat: work with dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,9 @@ module.exports = {
             },
         ],
     },
+    devtool: "eval-cheap-module-source-map",
+
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+    },
 };
